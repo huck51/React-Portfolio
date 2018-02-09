bf85bf7022a7c72c4b2d1688f242215a32e94623
Huck <evanallen51@gmail.com>
2018-02-09 17:45:22 -0600

    Deleted unused photos and updated campfire stories photo

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { Row, Col, Thumbnail } from 'react-bootstrap';
 import jsPiano from '../Images/jspiano.png';
-import campground from '../Images/CG.png';
+import campground from '../Images/CampfireStories.png';
 import bricks from '../Images/bricks.jpeg';
 import arc_hive from '../Images/arc_hive1.jpg';
 import '../App.css';
@@ -104,3 +104,7 @@ class Projects extends Component {
 }
 
 export default Projects;
+
+Arc_Hive is an educational tool intended for use at Lambda School that automates the instructors lengthy process of creating a Zoom lecture, sharing the access link in Slack, converting the recorded video file type, uploading the video to YouTube, sharing the YouTube link with students in Slack, and entering the link and corresponding lecture information into AirTable. We automated this process by creating a Slack app and server which allows the instructor to use a slash command in Slack to enter all the metadata for the lecture (title, tags, class, ect.) from there our server takes over and automates the entire process listed above, leaving the teacher free to focus on doing what they do best, teaching. This tool also includes a Slack command for students which allows them to query AirTable using content tags and class names. The titles and links of all lectures matching the search parameters are then returned to the student in Slack via a direct message.  Arc_Hive was built using Node.js and Express.js. It utilizes the Slack API, Zoom API, Google/YouTube API,  and the AirTable API.
+
+I set up the server on Heroku and connected it with the Slack API, Zoom API, YouTube API, and Airtable API. Connecting with the APIs required me to implement several authorization techniques including JWT and OAuth2. I was also responsible for writing a ton of server code that directed the flow of data from one API to the next, repackaging the data when necessary. In addition to contributing code, I participated in project planning meetings and daily stand-ups.
