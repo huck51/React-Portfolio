1a12172312bb0641a230ba92ac96d4b3c01d28fe
Huck <evanallen51@gmail.com>
2018-02-10 02:36:38 -0600

    Fixed safari grid issues

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -25,9 +25,9 @@ class Projects extends Component {
               responsive="true">
                 <hr className="thumbnailHr"/>
                 <h4>JavaScript Piano</h4>
-                <input type="checkbox" class="read-more-state" id="post-1" />
-                <label for="post-1" class="read-more-trigger"></label>
-                <p id="description" class="read-more-wrap"><span class="read-more-target">A piano keyboard created using JavaScript and the Howler.js
+                <input type="checkbox" className="read-more-state" id="post-1" />
+                <label for="post-1" className="read-more-trigger"></label>
+                <p id="description" className="read-more-wrap"><span className="read-more-target">A piano keyboard created using JavaScript and the Howler.js
                 library. The piano is played using the computer keyboard. When
                 a key is pressed the note is played and the corresponding key
                 lights up.</span></p>
@@ -43,9 +43,9 @@ class Projects extends Component {
               responsive="true">
                 <hr className="thumbnailHr"/>
                 <h4>Campfire Stories</h4>
-                <input type="checkbox" class="read-more-state" id="post-2" />
-                <label for="post-2" class="read-more-trigger"></label>
-                <p class="read-more-wrap"><span class="read-more-target">Campfire Stories is a review site dedicated to reviewing
+                <input type="checkbox" className="read-more-state" id="post-2" />
+                <label for="post-2" className="read-more-trigger"></label>
+                <p className="read-more-wrap"><span className="read-more-target">Campfire Stories is a review site dedicated to reviewing
                 campgrounds, hiking trails, and national parks. It is inspired
                 by Yelp and built using Node.js, Express.js, MongoDB, and
                 Embedded JavaScript. Additionally, Campfire Stories uses
@@ -64,9 +64,9 @@ class Projects extends Component {
               responsive="true">
                 <hr className="thumbnailHr"/>
                 <h4>Arc_Hive</h4>
-                <input type="checkbox" class="read-more-state" id="post-3" />
-                <label for="post-3" class="read-more-trigger"></label>
-                <p class="read-more-wrap"><span class="read-more-target">Arc_Hive is an educational tool intended for use at Lambda
+                <input type="checkbox" className="read-more-state" id="post-3" />
+                <label for="post-3" className="read-more-trigger"></label>
+                <p className="read-more-wrap"><span className="read-more-target">Arc_Hive is an educational tool intended for use at Lambda
                   School that automates the instructors lengthy process of
                   creating a Zoom lecture, sharing the access link in Slack,
                   converting the recorded video file type, uploading the video
@@ -95,9 +95,9 @@ class Projects extends Component {
               responsive="true">
                 <hr className="thumbnailHr"/>
                 <h4>Wander Outdoor Mobile App - Coming Soon!</h4>
-                <input type="checkbox" class="read-more-state" id="post-4" />
-                <label for="post-4" class="read-more-trigger"></label>
-                <p class="read-more-wrap"><span class="read-more-target">Check back soon!</span></p>
+                <input type="checkbox" className="read-more-state" id="post-4" />
+                <label for="post-4" className="read-more-trigger"></label>
+                <p className="read-more-wrap"><span className="read-more-target">Check back soon!</span></p>
             </Thumbnail>
           </Col>
         </Row>
