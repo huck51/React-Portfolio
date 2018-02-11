425b4d2997551f9248e02f095294fa0b62b942bc
Huck <evanallen51@gmail.com>
2018-02-11 14:58:19 -0600

    Slide style 0.0

--- portfolio/src/Components/Slider.js
+++ portfolio/src/Components/Slider.js
@@ -13,32 +13,40 @@ class Slider extends Component {
       <Carousel className="carousel-fade container-fluid">
         <Carousel.Item animateIn animateOut>
           <div id="test1">
+            <div className="opac">
+            </div>
           </div>
           {/*<Image width={900} height={500} alt="900x500" className="carImg" src={fogForest} />*/}
           <Carousel.Caption className="slideOne jumbotron">
             <div className="container-fluid">
-              <h1 id="first">Hi, Im Evan. Part hockey fan. Part outdoorsman. Full Stack Developer.</h1>
+              <h1 className="slideHead">EVAN ALLEN</h1>
+              <hr className="spacer"/>
+              <h1 className="slideHead">FULL STACK DEVELOPER</h1>
               <p></p>
             </div>
           </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item animateIn animateOut>
         <div id="test2">
+          <div className="opac">
+          </div>
         </div>
         {/*<Image width={900} height={500} alt="900x500" className="carImg" src={stones} />*/}
         <Carousel.Caption className="slideTwo jumbotron">
           <div className="container-fluid">
-            <h1 id="second">I like to build cool things that have an impact on the world</h1>
+            <h1 className="slideHead">I like to build cool things that have an impact on the world</h1>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item animateIn animateOut>
         <div id="test3">
+          <div className="opac">
+          </div>
         </div>
         {/*<Image width={900} height={500} alt="900x500" className="carImg" src={rockStream} />*/}
         <Carousel.Caption className="slideThree jumbotron">
           <div className="container-fluid">
-            <h1 id="third">I love learning new languages, tools, and technologies!</h1>
+            <h1 className="slideHead">I love learning new languages, tools, and technologies!</h1>
             <p></p>
           </div>
         </Carousel.Caption>
