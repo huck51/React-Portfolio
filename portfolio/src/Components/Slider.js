254114f1f3feafc0a9b08983e38d8f0076859a95
Huck <evanallen51@gmail.com>
2018-02-12 19:23:52 -0600

    Final stage slide styling

--- portfolio/src/Components/Slider.js
+++ portfolio/src/Components/Slider.js
@@ -13,13 +13,11 @@ class Slider extends Component {
       <Carousel className="carousel-fade container-fluid">
         <Carousel.Item animateIn animateOut>
           <div id="test1">
-            <div className="opac">
-            </div>
           </div>
           {/*<Image width={900} height={500} alt="900x500" className="carImg" src={fogForest} />*/}
-          <Carousel.Caption className="slideOne jumbotron">
-            <div className="container-fluid">
-              <h1 className="slideHead">EVAN ALLEN</h1>
+          <Carousel.Caption>
+            <div className="slideFour">
+              <h1 id="evan" className="slideHead">EVAN ALLEN</h1>
               <hr className="spacer"/>
               <h1 className="slideHead">FULL STACK DEVELOPER</h1>
               <p></p>
@@ -28,24 +26,25 @@ class Slider extends Component {
       </Carousel.Item>
       <Carousel.Item animateIn animateOut>
         <div id="test2">
-          <div className="opac">
+          <div className="">
           </div>
         </div>
         {/*<Image width={900} height={500} alt="900x500" className="carImg" src={stones} />*/}
-        <Carousel.Caption className="slideTwo jumbotron">
-          <div className="container-fluid">
+        <Carousel.Caption>
+          <div className="slideFour">
             <h1 className="slideHead">I like to build cool things that have an impact on the world</h1>
+            <p></p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item animateIn animateOut>
         <div id="test3">
-          <div className="opac">
+          <div className="">
           </div>
         </div>
         {/*<Image width={900} height={500} alt="900x500" className="carImg" src={rockStream} />*/}
-        <Carousel.Caption className="slideThree jumbotron">
-          <div className="container-fluid">
+        <Carousel.Caption className="">
+          <div className="slideFour">
             <h1 className="slideHead">I love learning new languages, tools, and technologies!</h1>
             <p></p>
           </div>
