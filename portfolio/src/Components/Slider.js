528f6113a2cc5728f252e175b8af7b797d9855a2
Huck <evanallen51@gmail.com>
2018-02-13 23:19:47 -0600

    Firefox fix

--- portfolio/src/Components/Slider.js
+++ portfolio/src/Components/Slider.js
@@ -9,53 +9,69 @@ import '../App.css'
 class Slider extends Component {
 
   render() {
-    return (
-      <Carousel className="carousel-fade container-fluid">
+    if (typeof InstallTrigger !== 'undefined') {
+      return (
+        <div className="carousel-fade container-fluid carousel slide">
+          <div className="carousel-inner">
+            <div id="test1">
+            </div>
+            {/*<Image width={900} height={500} alt="900x500" className="carImg" src={fogForest} />*/}
+            <Carousel.Caption>
+              <div className="slideFourOne">
+                <h1 id="evan" className="slideHead">EVAN ALLEN</h1>
+                <hr className="spacer"/>
+                <h1 className="slideHead">FULL STACK DEVELOPER</h1>
+                <p></p>
+              </div>
+            </Carousel.Caption>
+        </div>
+      </div>
+      );
+    } else {
+      return (
+        <Carousel className="carousel-fade container-fluid">
+          <Carousel.Item animateIn animateOut>
+            <div id="test1">
+            </div>
+            {/*<Image width={900} height={500} alt="900x500" className="carImg" src={fogForest} />*/}
+            <Carousel.Caption>
+              <div className="slideFourOne">
+                <h1 id="evan" className="slideHead">EVAN ALLEN</h1>
+                <hr className="spacer"/>
+                <h1 className="slideHead">FULL STACK DEVELOPER</h1>
+                <p></p>
+              </div>
+            </Carousel.Caption>
+        </Carousel.Item>
         <Carousel.Item animateIn animateOut>
-          <div id="test1">
+          <div id="test2">
           </div>
-          {/*<Image width={900} height={500} alt="900x500" className="carImg" src={fogForest} />*/}
-          <Carousel.Caption >
-            <div className="slideFourOne">
-              <h1 id="evan" className="slideHead">EVAN ALLEN</h1>
-              <hr className="spacer"/>
-              <h1 className="slideHead">FULL STACK DEVELOPER</h1>
+          {/*<Image width={900} height={500} alt="900x500" className="carImg" src={stones} />*/}
+          <Carousel.Caption>
+            <div className="slideFour">
+              <hr className="overUnderBar"/>
+              <h1 className="slideHeadTwo">I like to build cool things that have an impact on the world</h1>
+              <hr className="overUnderBar"/>
               <p></p>
             </div>
           </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item animateIn animateOut>
-        <div id="test2">
-          <div className="">
+        </Carousel.Item>
+        <Carousel.Item animateIn animateOut>
+          <div id="test3">
           </div>
-        </div>
-        {/*<Image width={900} height={500} alt="900x500" className="carImg" src={stones} />*/}
-        <Carousel.Caption>
-          <div className="slideFour">
-            <hr className="overUnderBar"/>
-            <h1 className="slideHeadTwo">I like to build cool things that have an impact on the world</h1>
-            <hr className="overUnderBar"/>
-            <p></p>
-          </div>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item animateIn animateOut>
-        <div id="test3">
-          <div className="">
-          </div>
-        </div>
-        {/*<Image width={900} height={500} alt="900x500" className="carImg" src={rockStream} />*/}
-        <Carousel.Caption className="">
-          <div className="slideFour">
-            <hr className="overUnderBar"/>
-            <h1 className="slideHeadThree">I love learning new languages, tools, and technologies!</h1>
-            <hr className="overUnderBar"/>
-            <p></p>
-          </div>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-    );
+          {/*<Image width={900} height={500} alt="900x500" className="carImg" src={rockStream} />*/}
+          <Carousel.Caption>
+            <div className="slideFour">
+              <hr className="overUnderBar"/>
+              <h1 className="slideHeadThree">I love learning new languages, tools, and technologies!</h1>
+              <hr className="overUnderBar"/>
+              <p></p>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      );
+    }
   }
 }
 
