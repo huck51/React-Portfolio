00f969f810bcd00ad1677867f18adbd7e287d52c
Huck <evanallen51@gmail.com>
2018-02-15 01:31:42 -0600

    addressing feedback, fixing errors, and styling 0.2

--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -36,36 +36,36 @@ class Contact extends Component {
                 </p>*/}
               </Col>
             </Row>
-            <Row>
-              <Col xs={12} sm={12} md={6} lg={3} className="noPadding">
-                  <Button className="contactButtons resume"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://resume.creddle.io/resume/7cix8cvqexc"
-                  ><i className="fa fa-file-text-o" aria-hidden="true"></i> Resume</Button>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={3} className="noPadding">
-                  <Button className="contactButtons gitHub"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://github.com/huck51"
-                  ><i className="fa fa-github" aria-hidden="true"></i> GitHub</Button>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={3} className="noPadding">
-                  <Button className="contactButtons linkedIn"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.linkedin.com/in/evan-allen-1b533864/"
-                  ><i className="fa fa-linkedin-square" aria-hidden="true"></i> LinkedIn</Button>
-              </Col>
-              <Col xs={12} sm={12} md={6} lg={3} className="noPadding">
-                  <Button className="contactButtons fcc"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.freecodecamp.org/huck51"
-                  ><i className="fa fa-free-code-camp" aria-hidden="true"></i> FreeCodeCamp</Button>
-              </Col>
-            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} sm={6} md={3} lg={3} className="noPadding">
+              <Button className="contactButtons resume"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://resume.creddle.io/resume/7cix8cvqexc"
+              ><i className="fa fa-file-text-o" aria-hidden="true"></i> Resume</Button>
+          </Col>
+          <Col xs={12} sm={6} md={3} lg={3} className="noPadding">
+              <Button className="contactButtons gitHub"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/huck51"
+              ><i className="fa fa-github" aria-hidden="true"></i> GitHub</Button>
+          </Col>
+          <Col xs={12} sm={6} md={3} lg={3} className="noPadding">
+              <Button className="contactButtons linkedIn"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/evan-allen-1b533864/"
+              ><i className="fa fa-linkedin-square" aria-hidden="true"></i> LinkedIn</Button>
+          </Col>
+          <Col xs={12} sm={6} md={3} lg={3} className="noPadding">
+              <Button className="contactButtons fcc"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.freecodecamp.org/huck51"
+              ><i className="fa fa-free-code-camp" aria-hidden="true"></i> FreeCodeCamp</Button>
           </Col>
         </Row>
       </div>
